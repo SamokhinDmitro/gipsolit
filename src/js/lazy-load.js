d72dbb8436409467707f082b10a890a93b0d20b4
SamokhinDmitro <samokhin.dmitriy.dev@gmail.com>
2021-01-23 14:17:48 +0200

    Lazy load script

---
+++ src/js/lazy-load.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", function () {
+    [].forEach.call(document.querySelectorAll("img[data-src]"), function (t) {
+        t.setAttribute("src", t.getAttribute("data-src")),
+            (t.onload = function () {
+                t.removeAttribute("data-src");
+            });
+    });
+});
